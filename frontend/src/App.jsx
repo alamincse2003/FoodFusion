e8e22b0ff9cac1f9d6627a0d9b6b889e81bd0798
alamincse2003 <mdalamincse2003@gmail.com>
2025-01-09 01:34:20 +0600

    frontend part initial setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-red-500 bg-black text-center font-bold">Food Fusion</h1>
+    </>
+  )
+}
+
+export default App
